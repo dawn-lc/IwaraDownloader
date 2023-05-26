20ef7d8e54c79faa7429659474a30e61ae6a24cd
dawn-lc <lc990930@163.com>
2023-05-26 14:07:58 +0800

    清理项目文件

--- IwaraDownloader/Utils.cs
+++ IwaraDownloader/Utils.cs
@@ -2,6 +2,7 @@
 using Spectre.Console;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using static IwaraDownloader.Config;
@@ -12,11 +13,12 @@ namespace Dawnlc.Module
     {
         public static class Env
         {
+            private static readonly Version version = Assembly.GetExecutingAssembly().GetName().Version ?? new(1,0,0);
             public static readonly string Name = "IwaraDownloader";
             public static readonly string Path = AppDomain.CurrentDomain.BaseDirectory;
             public static readonly string Developer = "dawn-lc";
             public static readonly string HomePage = $"https://github.com/{Developer}/{Name}";
-            public static readonly int[] Version = new int[] { 1, 0, 0 };
+            public static readonly int[] Version = new int[] { version.Major, version.Minor, version.Build };
             public static readonly HttpHeaders Headers = new HttpClient().DefaultRequestHeaders;
             public static Config MainConfig { get; set; } = DeserializeJSONFile<Config>(System.IO.Path.Combine(Path, "config.json"));
         }
